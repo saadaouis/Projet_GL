6b5547cd09451c11c510972cc5d1de3deb912a86
Zaidoudou <coskun.achile@gmail.com>
2025-05-09 16:00:21 +0200

    main setup

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// <copyright file="Program.cs" company="EasySave">
+// Copyright (c) EasySave. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace EasySave
+{
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
