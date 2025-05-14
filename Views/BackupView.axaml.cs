4d12053872e0bc04e69675df933ab50837da6f0c
Zaidoudou <coskun.achile@gmail.com>
2025-05-14 11:57:38 +0200

    fixed linter errors

--- Views/BackupView.axaml.cs
+++ Views/BackupView.axaml.cs
@@ -1,12 +1,22 @@
+// <copyright file="BackupView.axaml.cs" company="EasySave">
+// Copyright (c) EasySave. All rights reserved.
+// </copyright>
+
 using Avalonia.Controls;
 
 namespace EasySave.Views
 {
+    /// <summary>
+    /// The backup view.
+    /// </summary>
     public partial class BackupView : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BackupView"/> class.
+        /// </summary>
         public BackupView()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 } 
