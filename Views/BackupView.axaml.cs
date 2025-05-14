8c54686139a16f9b9fd7b223f4817e906a191cae
Zaidoudou <coskun.achile@gmail.com>
2025-05-14 11:54:18 +0200

    Feature: Aded BackupView.axaml.Cs file to properly intianilized BackupView page

---
+++ Views/BackupView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EasySave.Views
+{
+    public partial class BackupView : UserControl
+    {
+        public BackupView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
