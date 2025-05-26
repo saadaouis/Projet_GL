c143a1af7bbbaace2064a5e2a89ecdf40b534d32
Zaidoudou <coskun.achile@gmail.com>
2025-05-26 14:43:39 +0200

    FIX : TranslationManager linting errors

--- services/translation/TranslationManager.cs
+++ services/translation/TranslationManager.cs
@@ -1,17 +1,25 @@
-using System;
-using System.Windows.Input;
+// <copyright file="TranslationManager.cs" company="EasySave">
+// Copyright (c) EasySave. All rights reserved.
+// </copyright>
 
 namespace EasySave.Services.Translation
 {
-    public class TranslationManager
+    /// <summary>
+    /// Translation manager class for handling translations.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="TranslationManager"/> class.
+    /// </remarks>
+    /// <param name="translationService">The translation service.</param>
+    public class TranslationManager(TranslationService translationService)
     {
-        private readonly TranslationService translationService;
+        private readonly TranslationService translationService = translationService;
 
-        public TranslationManager(TranslationService translationService)
-        {
-            this.translationService = translationService;
-        }
-
+        /// <summary>
+        /// Gets the translation for a given key.
+        /// </summary>
+        /// <param name="key">The key to get the translation for.</param>
+        /// <returns>The translation for the given key.</returns>
         public string this[string key] => this.translationService.GetTranslation(key);
     }
 } 
